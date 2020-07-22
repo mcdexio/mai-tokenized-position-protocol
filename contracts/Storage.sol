7d446cdaaa0ca76ac91fcceccaaefcb2e93174b6
zhutianchi <zhutianchi@mcarlo.com>
2020-07-22 17:52:29 +0800

    Revert "treat liquidated situations"
    This reverts commit 5ff86a39beb69e95def477d65e16fce03faccbb3.

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -5,25 +5,17 @@ pragma solidity 0.6.10;
 library LibTypes {
     enum Side {FLAT, SHORT, LONG}
     enum Status {NORMAL, EMERGENCY, SETTLED}
-    struct MarginAccount {
-        LibTypes.Side side;
-        uint256 size;
-        uint256 entryValue;
-        int256 entrySocialLoss;
-        int256 entryFundingLoss;
-        int256 cashBalance;
-    }
 }
 
 // see https://github.com/mcdexio/mai-protocol-v2/blob/master/contracts/interface/IPerpetual.sol
 interface IPerpetual {
     function collateral() external view returns (address);
-    function getMarginAccount(address trader) external view returns (LibTypes.MarginAccount memory);
     function markPrice() external returns (uint256);
     function status() external view returns (LibTypes.Status);
     function marginBalance(address trader) external returns (int256);
     function isSafe(address trader) external returns (bool);
     function isIMSafe(address trader) external returns (bool);
+    function isValidTradingLotSize(uint256 amount) external view returns (bool);
     function depositFor(address trader, uint256 rawAmount) external payable;
     function withdrawFor(address payable trader, uint256 rawAmount) external;
     function transferCashBalance(
